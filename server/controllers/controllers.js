7b6c64ee1d3bd1b7f3340dc8f272c7bcd0bbd0e1
srivastav <srivastavkancharala@gmail.com>
2025-04-13 11:23:44 +0530

    redis congigured

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -4,7 +4,7 @@ import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import Redis from "ioredis";
 
-const redis = new Redis("redis://redis:6379");
+const redis = new Redis(process.env.REDIS_URL);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
